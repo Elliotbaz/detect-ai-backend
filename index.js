41f8d20df7dd73310ff534dfd3f0316b961786ee
Elliot Bazuaye <obazuaye55@gmail.com>
2023-03-28 23:02:26 +0100

    server

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 const configuration = new Configuration({
-    apiKey: process.env.OPEN_AI_KEY
+    apiKey: process.env.API_KEY
 })
 
 const openAI = new OpenAIApi(configuration)
